df85e11beaa6091d4e9594f792598626e3a581a3
ryedunn <ryedunn@hotmail.com>
2021-08-19 17:01:38 -0500

    Refactoring

--- tk_ToolTip.py
+++ tk_ToolTip.py
@@ -3,11 +3,13 @@ import tkinter as tk
 # Written by vegaset
 # https://www.daniweb.com/programming/software-development/code/484591/a-tooltip-class-for-tkinter
 
+
 class CreateToolTip(object):
-    '''
+    """
     create a tooltip for a given widget
-    '''
-    def __init__(self, widget, text='widget info'):
+    """
+
+    def __init__(self, widget, text="widget info"):
         self.widget = widget
         self.text = text
         self.widget.bind("<Enter>", self.enter)
@@ -26,9 +28,16 @@ class CreateToolTip(object):
         self.tw.wm_overrideredirect(True)
         self.tw.wm_geometry("+%d+%d" % (x, y))
 
-        label = tk.Label(self.tw, text=self.text, justify='left', foreground="white",
-                       background='dimgray', relief='solid', borderwidth=0,
-                       font=("Helvetica", "9", "bold"))
+        label = tk.Label(
+            self.tw,
+            text=self.text,
+            justify="left",
+            foreground="white",
+            background="dimgray",
+            relief="solid",
+            borderwidth=0,
+            font=("Helvetica", "9", "bold"),
+        )
         label.pack(ipadx=1)
 
     def close(self, event=None):
